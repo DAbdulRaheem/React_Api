7c19ccb4982df59ddfed60e27d33ab9d99e46f0c
DAbdulRaheem <raheemabduld56@gmail.com>
2025-12-05 22:59:49 +0530

    modified database

--- Blogs/Blog_Api/Blog_Api/settings.py
+++ Blogs/Blog_Api/Blog_Api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-pep3+gz-#ga8row6kvy)cjxx_(7$-ein7#a=!um)e-^$gjn#g9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
